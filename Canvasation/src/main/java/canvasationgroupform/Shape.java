41d7dfd5539371d20dc51cda57b2a27ad2dcdcf3
Tanner Chauncy <tanna1235@gmail.com>
2018-04-21 11:56:46 -0500

    Add multi-user version
    Has multi-user availability

---
+++ Canvasation/src/main/java/canvasationgroupform/Shape.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package canvasationgroupform;
+
+import java.awt.Graphics;
+import java.awt.Point;
+
+/**
+ *
+ * @author christian_white
+ */
+public abstract class Shape {
+    public abstract void draw(Graphics g, Point drawAt, int size);
+}
